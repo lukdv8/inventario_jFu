e245f883936a66db232c16d44f3b876c2b0fafe3
Lucas Duarte <lukvig@hotmail.com>
2024-07-12 18:49:26 -0300

    feat(Produto): adiciona inventario e status

--- ProductTester.java
+++ ProductTester.java
@@ -1,18 +1,62 @@
+import java.util.Scanner;
 
 public class ProductTester {
     public static void main(String[] args) {
-        Produto corrente1 = new Produto("Yaban Index 24v", 80.0, 5, 1);
-        Produto corrente2 = new Produto("Yaban Comum 21v", 60.0, 5, 2);
-        Produto catraca3 = new Produto("Catraca de 6", 60.0, 20, 3);
-        Produto catraca4 = new Produto("Catraca de 7", 70.0, 20, 4);
-        Produto vbrakeAlum = new Produto();
-        Produto vbrakeNylon = new Produto();
+    	/*String tempNome;
+    	double tempPreco;
+    	int tempQtde;
+    	int tempCod;
+    	boolean tempAtivo = true;
+    	
+    	
+    	Scanner in = new Scanner (System.in);
+    	
+    	System.out.print("Nome: ");
+    	tempNome = in.nextLine();
+    	
+    	System.out.print("Valor: ");
+    	tempPreco = in.nextDouble();
+    	
+    	System.out.print("Quantidade: ");
+    	tempQtde = in.nextInt();
+    	
+    	System.out.print("Codigo: ");
+    	tempCod = in.nextInt();
+
+        Produto vbrakeNylon = new Produto(tempNome, tempPreco, tempQtde, tempCod, tempAtivo);
+
+        in.nextLine();
+
+        System.out.print("Nome: ");
+    	tempNome = in.nextLine();
+    	
+    	System.out.print("Valor: ");
+    	tempPreco = in.nextDouble();
+    	
+    	System.out.print("Quantidade: ");
+    	tempQtde = in.nextInt();
+    	
+    	System.out.print("Codigo: ");
+    	tempCod = in.nextInt();
+        in.close();
+
+        Produto vbrakeAlum = new Produto(tempNome, tempPreco, tempQtde, tempCod, tempAtivo);
+        
+        //vbrakeAlum.setEstaAtivo(false);*/
+    	
+        Produto corrente1 = new Produto("Yaban Index 24v", 80.0, 5, 1, true);
+        Produto corrente2 = new Produto("Yaban Comum 21v", 60.0, 5, 2, true);
+        Produto catraca3 = new Produto("Catraca de 6", 60.0, 20, 3, true);
+        Produto catraca4 = new Produto("Catraca de 7", 70.0, 20, 4, true);
 
         System.out.println(corrente1);
         System.out.println(corrente2);
         System.out.println(catraca3);
         System.out.println(catraca4);
-        System.out.println(vbrakeAlum);
-        System.out.println(vbrakeNylon);
+        catraca4.setQtdeEstoque(0);
+        System.out.println(catraca4);
+        //System.out.println(vbrakeNylon);
+        //System.out.println(vbrakeAlum);
+
     }
 }
--- Produto.java
+++ Produto.java
@@ -5,18 +5,20 @@ public class Produto {
     private double valorProduto;
     private int qtdeEstoque;
     private int codItem;
+    private boolean estaAtivo = true;
 
     //construtor padrão
     public Produto (){
-
+    	
     }
 
     //contrutor com parâmetros de instância
-    public Produto (String nome, double valor, int qtdeEstoque, int codigo){
-        this.nomeProduto = nome;
-        this.valorProduto = valor;
+    public Produto (String nomeProduto, double valorProduto, int qtdeEstoque, int codItem, boolean estaAtivo){
+        this.nomeProduto = nomeProduto;
+        this.valorProduto = valorProduto;
         this.qtdeEstoque = qtdeEstoque;
-        this.codItem = codigo;
+        this.codItem = codItem;
+        this.estaAtivo = estaAtivo;
     }
 
     //métodos de acesso e modificadores
@@ -24,39 +26,58 @@ public class Produto {
         return nomeProduto;
     }
 
-    public void setNomeProduto (){
-
+    public void setNomeProduto (String nomeProduto){
+    	this.nomeProduto = nomeProduto;
     }
 
     public double getValorProduto (){
         return valorProduto;
     }
 
-    public void setValorProduto (){
-
+    public void setValorProduto (double valorProduto){
+    	this.valorProduto = valorProduto;
     }
 
     public int getQtdeEstoque (){
         return qtdeEstoque;
     }
 
-    public void setQtdeEstoque (){
-
+    public void setQtdeEstoque (int qtdeEstoque){
+    	this.qtdeEstoque = qtdeEstoque;
     }
 
     public int getCodItem (){
         return codItem;
     }
 
-    public void setCodItem (){
+    public void setCodItem (int codItem){
+    	this.codItem = codItem;
+    }
 
+    public boolean getEstaAtivo (){
+        return estaAtivo;
+    }
+
+    public void setEstaAtivo (boolean estaAtivo){
+        this.estaAtivo = estaAtivo;
+    	/*if (qtdeEstoque > 0) {
+    		estaAtivo = true;
+    	} else {
+    		estaAtivo = false;
+    	}*/
+    }
+    
+    public double getInventaryValue () {
+    	return getValorProduto() * getQtdeEstoque();
     }
 
     @Override
     public String toString() {
-        return  "Item: " + getNomeProduto() + 
+        return  "Item: " + getNomeProduto() +
+        		"; Código: " + getCodItem() + 
+                "; Valor: $" + getValorProduto() + 
                 "; Quantidade: " + getQtdeEstoque() + 
-                "; Código: " + getCodItem() + 
-                "; Valor: " + getValorProduto();
+                "; Valor do estoque: " + getInventaryValue() + 
+                "; Status do produto: " + (getQtdeEstoque() > 0 ? "Ativo" : "Descontinuado");
     }
 }
